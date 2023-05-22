d23781ae6e5df4ee4e1001a0e4538155690c3c0f
Haider Zaman <106913229+haiderzamanusafxay@users.noreply.github.com>
2023-05-22 03:38:52 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// DESIGNED AND DEVELOPED BY ANAMZREDIAH
+
+// js code 
+const navbar = document.querySelector('.header .navbar');
+const menuButton = document.querySelector('.header .menu');
+
+menuButton.addEventListener('click', () => {
+  navbar.classList.toggle('show');
+  menuButton.classList.toggle('fa-close');
+});
+
+document.onscroll = () => {
+  navbar.classList.remove('show');
+  menuButton.classList.remove('fa-close');
+
+  if (window.scrollY > 0) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+};
+
+document.onload = () => {
+  if (window.scrollY > 10) {
+    document.querySelector('.header').classList.add('active');
+  } else {
+    document.querySelector('.header').classList.remove('active');
+  }
+  
+};
+
+// jQuery Code 
+$(document).ready(function(){
+
+});
+
+
+
