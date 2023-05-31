8a592971e425d276b0c2e3e62691a4b5c0e23029
HaiderZaman <haiderzamanusafxay@gmail.com>
2023-05-31 15:13:31 -0700

    made new changes

--- emaildata.php
+++ emaildata.php
@@ -1,23 +1,23 @@
 <?php
 // if (isset($_POST['submit'])) {
     
-//     $to = "digitaltopspot1@gmail.com";
+//     $to = "haiderzamanusafxay@gmail.com";
 //     $email= $_POST['email'];
 //     $subject= "Website Email From :". $email;
 //     $name= $_POST['name'];
 //     $message= $_POST['message'];
 //     $header= "From ".$email;
 
-//     $result= mail($to, $subject,$message,$header);
+//     $result= mail($to, $subject,$message);
 //     if($result){
 //         header('index.php');
 //     }
 
 // }
-      $to = "digitaltopspot1@gmail.com";
-    //   $email= $_POST['email'];
-    //   $name= $_POST['name'];
-    //   $message= $_POST['message'];
+    $to = "haiderzamanusafxay@gmail.com";
+    $email= $_POST['email'];
+    $name= $_POST['name'];
+    $message= $_POST['message'];
     $headers =  'MIME-Version: 1.0' . "\r\n"; 
     $headers .= 'From: Your name digitaltopspot.com' . "\r\n";
     $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
@@ -25,11 +25,7 @@
       $subject= "new website email";
       $message= "hey";
 
-      $res= mail($to,$subject,$message);
-      if($res){
-        echo "success";
-      }else{
-        echo "failed";
-      }
+      mail($to,$subject,$message);
+      
 
 ?>
